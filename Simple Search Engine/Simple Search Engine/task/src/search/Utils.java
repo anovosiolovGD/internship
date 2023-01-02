98c702997bf8b658a0aa72b4a35116a707016331
Andrei Novosiolov <anovosiolov@griddynamics.com>
2023-01-02 18:08:06 +0200

    changed project

---
+++ Simple Search Engine/Simple Search Engine/task/src/search/Utils.java
@@ -0,0 +1,12 @@
+package search;
+
+import java.util.Scanner;
+
+public class Utils {
+
+    public static final Scanner generateScanner(){
+        //extract to here common code
+
+        return null;
+    }
+}
